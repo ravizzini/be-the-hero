e7f78b33cbf742f423000f091db594cb3b70576c
ravizzini <ravizzini@users.noreply.github.com>
2020-03-26 14:28:56 -0300

    2603201428

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 
@@ -10,6 +10,8 @@ import api from '../../services/api'
 
 export default function Profile(){
     const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
 
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
@@ -38,6 +40,11 @@ export default function Profile(){
         }
     }
 
+    function handleLogout(){
+        localStorage.clear();
+        history.push('./');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -45,7 +52,7 @@ export default function Profile(){
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
